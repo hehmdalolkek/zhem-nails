febadfead4092dc9e40362a458eda7931e45b59c
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-17 12:33:31 +0400

    init

---
+++ src/main/java/ru/zhem/ZhemNailsApplication.java
@@ -0,0 +1,13 @@
+package ru.zhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhemNailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhemNailsApplication.class, args);
+    }
+
+}
