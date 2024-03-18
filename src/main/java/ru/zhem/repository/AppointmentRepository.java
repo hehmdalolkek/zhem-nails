d57e01343c61ca353661dfde93f7270d7014b59d
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-18 23:14:21 +0400

    updated repositories

--- src/main/java/ru/zhem/repository/AppointmentRepository.java
+++ src/main/java/ru/zhem/repository/AppointmentRepository.java
@@ -1,7 +1,11 @@
 package ru.zhem.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.zhem.entity.Appointment;
 
+@Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+
+    Iterable<Appointment> findAllByUserId(Long userId);
 }
--- src/main/java/ru/zhem/repository/UserRepository.java
+++ src/main/java/ru/zhem/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package ru.zhem.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.zhem.entity.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 }
