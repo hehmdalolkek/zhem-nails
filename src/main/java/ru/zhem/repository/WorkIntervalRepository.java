4c508754d2bc5c39273c513b269a61498aa4df57
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-21 20:15:45 +0400

    deleted WorkDay

--- src/main/java/ru/zhem/repository/WorkIntervalRepository.java
+++ src/main/java/ru/zhem/repository/WorkIntervalRepository.java
@@ -2,15 +2,9 @@ package ru.zhem.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.zhem.entity.WorkDay;
 import ru.zhem.entity.WorkInterval;
 
 @Repository
 public interface WorkIntervalRepository extends CrudRepository<WorkInterval, Long> {
-
-    Iterable<WorkInterval> findWorkIntervalByWorkDay(WorkDay workDay);
-
-    Iterable<WorkInterval> findWorkIntervalsByIsBookedIsFalse();
-
-    Iterable<WorkInterval> findWorkIntervalsByWorkDayAndIsBookedIsFalse(WorkDay workDay);
+    Iterable<WorkInterval> findAllByOrderByDateDescStartTimeAsc();
 }
