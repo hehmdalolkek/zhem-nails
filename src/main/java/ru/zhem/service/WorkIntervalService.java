08b7f475e98f6f6dd4f357fe6836b536547c2072
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-21 20:18:49 +0400

    replaced WorkDay by LocalDate

--- src/main/java/ru/zhem/service/WorkIntervalService.java
+++ src/main/java/ru/zhem/service/WorkIntervalService.java
@@ -1,8 +1,8 @@
 package ru.zhem.service;
 
-import ru.zhem.entity.WorkDay;
 import ru.zhem.entity.WorkInterval;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
 
@@ -10,15 +10,11 @@ public interface WorkIntervalService {
 
     Iterable<WorkInterval> findWorkIntervals();
 
-    Iterable<WorkInterval> findWorkIntervalsByWorkDay(WorkDay workDay);
-
-    Iterable<WorkInterval> findWorkNotBookedIntervalsByWorkDay(WorkDay workDay);
-
     Optional<WorkInterval> findWorkInterval(long workIntervalId);
 
-    WorkInterval createWorkInterval(WorkDay workDay, LocalTime startTime);
+    WorkInterval createWorkInterval(LocalDate date, LocalTime startTime);
 
-    void updateWorkInterval(Long workIntervalId, WorkDay workDay, LocalTime startTime, Boolean isBooked);
+    void updateWorkInterval(long workIntervalId, LocalDate date, LocalTime startTime, Boolean isBooked);
 
     void deleteWorkInterval(long workIntervalId);
 
