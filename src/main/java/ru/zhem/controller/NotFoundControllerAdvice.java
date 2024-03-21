5b80cc32bc80860a7ae34c6bd45fc5107b6a542e
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-21 20:18:21 +0400

    added NotFoundControllerAdvice

---
+++ src/main/java/ru/zhem/controller/NotFoundControllerAdvice.java
@@ -0,0 +1,20 @@
+package ru.zhem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class NotFoundControllerAdvice {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ProblemDetail> handleNoSuchElementException(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ProblemDetail.forStatusAndDetail(
+                        HttpStatus.NOT_FOUND, exception.getMessage()));
+    }
+}
