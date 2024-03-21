a22d6f8963bc11072b22b66b703cb11a93e94f08
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-21 20:18:07 +0400

    added BadRequestControllerAdvice

---
+++ src/main/java/ru/zhem/controller/BadRequestControllerAdvice.java
@@ -0,0 +1,26 @@
+package ru.zhem.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class BadRequestControllerAdvice {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ProblemDetail> handlerBindException(BindException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST, exception.getMessage());
+        problemDetail.setProperty("errors", exception.getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .toList());
+
+        return ResponseEntity.badRequest()
+                .body(problemDetail);
+    }
+
+}
