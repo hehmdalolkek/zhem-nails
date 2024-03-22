85500f06a4edaaf09f60719125547be75ccdc4bb
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-22 23:41:22 +0400

    renamed ZhemNailsApplication

--- zhem-nails-service/src/main/java/ru/zhem/ZhemNailsServiceApplication.java
+++ zhem-nails-service/src/main/java/ru/zhem/ZhemNailsServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZhemNailsApplication {
+public class ZhemNailsServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZhemNailsApplication.class, args);
+        SpringApplication.run(ZhemNailsServiceApplication.class, args);
     }
 
 }
