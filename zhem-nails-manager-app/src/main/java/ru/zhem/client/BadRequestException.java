7761b72554742b3627be83140d3f05bdbc40a243
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-24 12:33:52 +0400

    added custom exception

---
+++ zhem-nails-manager-app/src/main/java/ru/zhem/client/BadRequestException.java
@@ -0,0 +1,18 @@
+package ru.zhem.client;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BadRequestException extends RuntimeException {
+
+    private final List<String> errors;
+
+    public BadRequestException(List<String> errors) {
+        this.errors = errors;
+    }
+
+}
