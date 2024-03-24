e369c0ee7f212451fb65b1d35482a360e27048ac
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-24 12:41:21 +0400

    released RestClient for WorkInterval

--- zhem-nails-manager-app/src/main/java/ru/zhem/client/RestClientWorkIntervalRestClient.java
+++ zhem-nails-manager-app/src/main/java/ru/zhem/client/RestClientWorkIntervalRestClient.java
@@ -2,54 +2,102 @@ package ru.zhem.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import ru.zhem.entity.WorkInterval;
+import ru.zhem.entity.payload.NewWorkIntervalPayload;
+import ru.zhem.entity.payload.UpdateWorkIntervalPayload;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 public class RestClientWorkIntervalRestClient implements WorkIntervalRestClient {
 
-    private static final ParameterizedTypeReference<List<WorkInterval>> WORK_INTERVALS_TYPE_REFERENCE =
+    private static final ParameterizedTypeReference<Map<LocalDate, List<WorkInterval>>> WORK_INTERVALS_TYPE_REFERENCE =
             new ParameterizedTypeReference<>() {
             };
 
     private final RestClient restClient;
 
     @Override
-    public List<WorkInterval> findAllWorkIntervals() {
-        return this.restClient
-                .get()
+    public Map<LocalDate, List<WorkInterval>> findAllWorkIntervals() {
+        return this.restClient.get()
                 .uri("/service-api/v1/workintervals")
                 .retrieve()
                 .body(WORK_INTERVALS_TYPE_REFERENCE);
     }
 
     @Override
-    public List<WorkInterval> findAvailableWorkIntervalsGroupedByDate() {
-        return null;
+    public Map<LocalDate, List<WorkInterval>> findAvailableWorkIntervalsGroupedByDate() {
+        return this.restClient.get()
+                .uri("/service-api/v1/workintervals/available")
+                .retrieve()
+                .body(WORK_INTERVALS_TYPE_REFERENCE);
     }
 
     @Override
     public Optional<WorkInterval> findWorkInterval(long workIntervalId) {
-        return Optional.empty();
+        try {
+            return Optional.ofNullable(
+                    this.restClient.get()
+                            .uri("/service-api/v1/workintervals/{workIntervalId}", workIntervalId)
+                            .retrieve()
+                            .body(WorkInterval.class)
+            );
+        } catch (HttpClientErrorException.NotFound exception) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public WorkInterval createWorkInterval(LocalDate date, LocalTime time) {
-        return null;
+        try {
+            return this.restClient.post()
+                    .uri("/service-api/v1/workintervals")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new NewWorkIntervalPayload(date, time))
+                    .retrieve()
+                    .body(WorkInterval.class);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
     public void updateWorkInterval(long workIntervalId, LocalDate date, LocalTime time) {
-
+        try {
+            this.restClient.patch()
+                    .uri("/service-api/v1/workintervals/{workIntervalId}", workIntervalId)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(new UpdateWorkIntervalPayload(date, time))
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpClientErrorException.NotFound exception) {
+            throw new NoSuchElementException(exception);
+        } catch (HttpClientErrorException.BadRequest exception) {
+            ProblemDetail problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+            throw new BadRequestException((List<String>) problemDetail.getProperties().get("errors"));
+        }
     }
 
     @Override
     public void deleteWorkInterval(long workIntervalId) {
-
+        try {
+            this.restClient
+                    .delete()
+                    .uri("/service-api/v1/workintervals/{workIntervalId}", workIntervalId)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpClientErrorException.NotFound exception) {
+            throw new NoSuchElementException(exception);
+        }
     }
 }
