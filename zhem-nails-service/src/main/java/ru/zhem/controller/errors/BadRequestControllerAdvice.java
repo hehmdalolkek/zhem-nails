6cd0bd06c5d67a48dec4588f4e348240d9c24b06
hehmdalolkek <badeha.inna@yandex.ru>
2024-03-26 20:35:33 +0400

    edited property errors in ProblemDetail

--- zhem-nails-service/src/main/java/ru/zhem/controller/errors/BadRequestControllerAdvice.java
+++ zhem-nails-service/src/main/java/ru/zhem/controller/errors/BadRequestControllerAdvice.java
@@ -4,9 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class BadRequestControllerAdvice {
@@ -14,10 +18,15 @@ public class BadRequestControllerAdvice {
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ProblemDetail> handlerBindException(BindException exception) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.BAD_REQUEST, exception.getMessage());
-        problemDetail.setProperty("errors", exception.getAllErrors().stream()
-                .map(ObjectError::getDefaultMessage)
-                .toList());
+                HttpStatus.BAD_REQUEST, "Bad credentials");
+
+        Map<String, String> errors = new HashMap<>();
+        for (Object error : exception.getAllErrors()) {
+            if (error instanceof FieldError fieldError) {
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+        }
+        problemDetail.setProperty("errors", errors);
 
         return ResponseEntity.badRequest()
                 .body(problemDetail);
