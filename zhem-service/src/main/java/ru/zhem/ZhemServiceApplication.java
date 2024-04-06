3a691d7055a225c0dcc227ebac7de4e13b392aaf
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-06 20:57:23 +0400

    init

---
+++ zhem-service/src/main/java/ru/zhem/ZhemServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.zhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhemServiceApplication.class, args);
+    }
+}
