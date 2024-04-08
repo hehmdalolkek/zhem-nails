81bb30b8dece7f704e324aa44435beaffeb5a954
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-08 23:12:32 +0400

    added Interval entity and Status enum for Interval

---
+++ zhem-service/src/main/java/ru/zhem/entity/Interval.java
@@ -0,0 +1,37 @@
+package ru.zhem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "zhem", name = "intervals")
+public class Interval extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "interval_id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "date")
+    private LocalDate date;
+
+    @NotNull
+    @Column(name = "time")
+    private LocalTime time;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status = Status.AVAILABLE;
+
+}
