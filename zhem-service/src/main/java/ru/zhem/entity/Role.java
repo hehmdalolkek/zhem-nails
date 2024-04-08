fa62ab42709bbd06d906125bf2267f85ab3d02fe
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-08 22:35:07 +0400

    added repository and exception for Role

---
+++ zhem-service/src/main/java/ru/zhem/entity/Role.java
@@ -0,0 +1,27 @@
+package ru.zhem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "zhem", name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 2, max = 32)
+    @Column(name = "title", unique = true)
+    private String title;
+
+}
---
+++ zhem-service/src/main/java/ru/zhem/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package ru.zhem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.zhem.entity.Role;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByTitleIgnoreCase(String title);
+
+}
