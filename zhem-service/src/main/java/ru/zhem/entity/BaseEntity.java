4fa8ee317b797e807b98a737f6051ab642e80b67
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-08 22:35:37 +0400

    added BaseEntity for all entities

---
+++ zhem-service/src/main/java/ru/zhem/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package ru.zhem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
