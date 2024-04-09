91203321008e4f82c145e51fdb14defb01a7328f
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-09 21:30:12 +0400

    added methods findAllBy and existsBy in repo for interval

--- zhem-service/src/main/java/ru/zhem/repository/IntervalRepository.java
+++ zhem-service/src/main/java/ru/zhem/repository/IntervalRepository.java
@@ -1,9 +1,30 @@
 package ru.zhem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.zhem.entity.Interval;
+import ru.zhem.entity.Status;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Repository
 public interface IntervalRepository extends JpaRepository<Interval, Long> {
+
+    @Query("from Interval i where extract(year from i.date) = :year and extract(month from i.date) = :month")
+    List<Interval> findAllByDateOrderByDateAscTimeDesc(@Param("year") Integer year, @Param("month") Integer month);
+
+    @Query("from Interval i where extract(year from i.date) = :year and extract(month from i.date) = :month" +
+            " and i.status = :status")
+    List<Interval> findAllByDateAndStatusOrderByDateAscTimeDesc(@Param("year") Integer year,
+                                                                @Param("month") Integer month,
+                                                                @Param("status") Status status);
+
+    boolean existsByDateAndTime(LocalDate date, LocalTime time);
+
+    boolean existsByDateAndTimeAndIdNot(LocalDate date, LocalTime time, long intervalId);
+
 }
