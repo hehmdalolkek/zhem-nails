92f6d28ab351ee902ac1155837ba071d6bb61251
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-10 22:41:02 +0400

    added appointment not found exception

---
+++ zhem-service/src/main/java/ru/zhem/exception/AppointmentNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.zhem.exception;
+
+public class AppointmentNotFoundException extends RuntimeException {
+    public AppointmentNotFoundException(String message) {
+        super(message);
+    }
+}
