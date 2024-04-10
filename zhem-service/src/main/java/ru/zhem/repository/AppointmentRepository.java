a8b7d5ec2ce2e9213676919978ae1a81d2687570
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-10 22:41:53 +0400

    added appointment repository

---
+++ zhem-service/src/main/java/ru/zhem/repository/AppointmentRepository.java
@@ -0,0 +1,30 @@
+package ru.zhem.repository;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.zhem.entity.Appointment;
+import ru.zhem.entity.Interval;
+import ru.zhem.entity.ZhemUser;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @EntityGraph(value = "appointment_entity-graph")
+    @Override
+    Optional<Appointment> findById(Long appointmentId);
+
+    @EntityGraph(value = "appointment_entity-graph")
+    List<Appointment> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+
+    @EntityGraph(value = "appointment_entity-graph")
+    @Query("from Appointment i where extract(year from i.interval.date) = :year and extract(month from i.interval.date) = :month" +
+            " order by i.interval.date, i.interval.time")
+    List<Appointment> findAllByIntervalDate(@Param("year") Integer year, @Param("month") Integer month);
+
+}
