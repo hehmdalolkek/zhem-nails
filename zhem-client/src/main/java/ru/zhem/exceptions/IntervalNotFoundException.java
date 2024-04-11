7a943fb00c5cb1075045c7b0187a5b607eec81a2
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-12 00:53:41 +0400

    added exception for interval

---
+++ zhem-client/src/main/java/ru/zhem/exceptions/IntervalNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.zhem.exceptions;
+
+public class IntervalNotFoundException extends RuntimeException {
+    public IntervalNotFoundException(String message) {
+        super(message);
+    }
+}
