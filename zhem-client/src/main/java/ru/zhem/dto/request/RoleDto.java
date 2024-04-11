a3b789064ecf0b680caa0e9f50b2a47aa49eb7a0
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-12 00:51:36 +0400

    dto for role

---
+++ zhem-client/src/main/java/ru/zhem/dto/request/RoleDto.java
@@ -0,0 +1,12 @@
+package ru.zhem.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RoleDto {
+
+    private Integer id;
+
+    private String title;
+
+}
---
+++ zhem-client/src/main/java/ru/zhem/dto/response/RoleCreationDto.java
@@ -0,0 +1,20 @@
+package ru.zhem.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleCreationDto {
+
+    @NotBlank(message = "Поле должно быть заполнено")
+    @Size(min = 2, max = 32, message = "Поле должно быть от 2 до 32 символов")
+    private String title;
+
+}
