26fa8c0b2a07b5f6ba9b1f86af44bd6994e4a1c5
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-12 00:52:11 +0400

    init

---
+++ zhem-client/src/main/java/ru/zhem/ZhemClientApplication.java
@@ -0,0 +1,12 @@
+package ru.zhem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhemClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhemClientApplication.class,args);
+    }
+
+}
---
+++ zhem-client/src/main/java/ru/zhem/config/WebConfig.java
@@ -0,0 +1,16 @@
+package ru.zhem.config;
+
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebSecurity
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/admin/dashboard").setViewName("admin/common/dashboard");
+    }
+
+}
