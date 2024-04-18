210da13391360a55767cb766674bf55d51d30ca4
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-18 22:51:17 +0400

    added NoResourceFoundException handler

--- zhem-client/src/main/java/ru/zhem/controller/errors/NotFoundControllerAdvice.java
+++ zhem-client/src/main/java/ru/zhem/controller/errors/NotFoundControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.zhem.exceptions.NotFoundException;
 
 @ControllerAdvice
@@ -21,5 +22,17 @@ public class NotFoundControllerAdvice {
         response.setStatus(HttpStatus.NOT_FOUND.value());
         return "common/errors/error";
     }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String handlerNoResourceFoundException(NoResourceFoundException exception, Model model,
+                                             HttpServletResponse response) {
+        ProblemDetail problemDetail = exception.getBody();
+        model.addAttribute("status", problemDetail.getStatus());
+        model.addAttribute("title", problemDetail.getTitle());
+        model.addAttribute("detail", problemDetail.getDetail());
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        return "common/errors/error";
+    }
+
 
 }
