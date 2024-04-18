827487b7ab42bb661486f5abc6eeedd5661350f8
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-19 00:02:02 +0400

    added intervals for all users

---
+++ zhem-client/src/main/java/ru/zhem/controller/common/CommonIntervalController.java
@@ -0,0 +1,40 @@
+package ru.zhem.controller.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.zhem.exceptions.BadRequestException;
+import ru.zhem.exceptions.InvalidDateException;
+import ru.zhem.service.CalendarService;
+import ru.zhem.service.IntervalService;
+
+import java.time.YearMonth;
+
+@RequiredArgsConstructor
+@Controller
+public class CommonIntervalController {
+
+    private final IntervalService intervalService;
+
+    private final CalendarService calendarService;
+
+    @GetMapping("/intervals")
+    public String initShowAvailableIntervals(@RequestParam(value = "year", required = false) Integer year,
+                                             @RequestParam(value = "month", required = false) Integer month, Model model) {
+        try {
+            YearMonth yearMonth = this.calendarService.calcPrevNextMonth(model, year, month);
+            model.addAttribute("mapOfIntervals",
+                    this.intervalService.generateIntervalCalendarForMonth(yearMonth, true));
+            return "common/intervals/intervals";
+        } catch (InvalidDateException exception) {
+            throw new BadRequestException(ProblemDetail.forStatusAndDetail(
+                    HttpStatus.BAD_REQUEST, "Invalid date"
+            ));
+        }
+    }
+
+}
