297600aab55762ea58e2d294c860e884e86b3b6f
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-24 11:58:47 +0400

    divided user services

--- zhem-client/src/main/java/ru/zhem/service/impl/ZhemUserDetailsService.java
+++ zhem-client/src/main/java/ru/zhem/service/impl/ZhemUserDetailsService.java
@@ -2,36 +2,23 @@ package ru.zhem.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Page;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.zhem.client.interfaces.RoleRestClient;
 import ru.zhem.client.interfaces.ZhemUserRestClient;
-import ru.zhem.dto.request.RoleDto;
 import ru.zhem.dto.request.ZhemUserAuthDto;
-import ru.zhem.dto.request.ZhemUserDto;
-import ru.zhem.dto.response.ZhemUserCreationDto;
-import ru.zhem.dto.response.ZhemUserUpdateDto;
-import ru.zhem.entity.ZhemUser;
-import ru.zhem.exceptions.RoleNotFoundException;
-import ru.zhem.service.interfaces.ZhemUserService;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Qualifier("zhemUserDetailsService")
 @Service
 @RequiredArgsConstructor
-public class ZhemUserDetailsService implements UserDetailsService, ZhemUserService {
+public class ZhemUserDetailsService implements UserDetailsService {
 
     private final ZhemUserRestClient zhemUserRestClient;
-
-    private final RoleRestClient roleRestClient;
 
     @Override
     public UserDetails loadUserByUsername(String phone) throws UsernameNotFoundException {
@@ -45,86 +32,5 @@ public class ZhemUserDetailsService implements UserDetailsService, ZhemUserServi
                 .password(user.getPassword())
                 .authorities(roles)
                 .build();
-    }
-
-    @Override
-    public List<ZhemUserDto> findAllClients() {
-        return this.zhemUserRestClient.findAllClients();
-    }
-
-    @Override
-    public Page<ZhemUserDto> findAllClientsByPage(int page, int size) {
-        return this.zhemUserRestClient.findAllClientsByPage(page, size);
-    }
-
-    @Override
-    public ZhemUserDto findUserById(Long id) {
-        return null;
-    }
-
-    @Override
-    public ZhemUserAuthDto findUserAuthByPhone(String phone, boolean isAdmin) {
-        return null;
-    }
-
-    @Override
-    public void createUser(ZhemUser user, boolean isAdmin) {
-        Set<RoleDto> roles = new HashSet<>();
-        roles.add(this.roleRestClient.findRoleByTitle("CLIENT")
-                .orElseThrow(() -> new RoleNotFoundException("Role not found")));
-        if (isAdmin) {
-            roles.add(this.roleRestClient.findRoleByTitle("ADMIN")
-                    .orElseThrow(() -> new RoleNotFoundException("Role not found")));
-        }
-        ZhemUserCreationDto userDto = ZhemUserCreationDto.builder()
-                .phone(user.getPhone())
-                .email(user.getEmail() != null && user.getEmail().isBlank() ? null : user.getEmail())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName() != null && user.getLastName().isBlank()
-                        && user.getLastName().length() < 2 ? null
-                        : user.getLastName())
-                .password(user.getPassword())
-                .roles(roles)
-                .build();
-        this.zhemUserRestClient.createUser(userDto);
-    }
-
-    @Override
-    public void updateUser(long userId, ZhemUser user) {
-        ZhemUserUpdateDto userDto = ZhemUserUpdateDto.builder()
-                .phone(user.getPhone())
-                .email(user.getEmail() != null && user.getEmail().isBlank() ? null : user.getEmail())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName() != null && user.getLastName().isBlank()
-                        && user.getLastName().length() < 2 ? null
-                        : user.getLastName())
-                .password(user.getPassword())
-                .build();
-        this.zhemUserRestClient.updateUser(userId, userDto);
-    }
-
-    @Override
-    public void deleteClient(long userId) {
-
-    }
-
-    @Override
-    public List<ZhemUserDto> findAllClientsBy(String firstName, String lastName, String phone, String email) {
-        return this.zhemUserRestClient.findAllClientsBy(firstName, lastName, phone, email);
-    }
-
-    @Override
-    public boolean adminIsExists() {
-        return this.zhemUserRestClient.adminIsExists();
-    }
-
-    @Override
-    public ZhemUserDto findUserByPhone(String phone) {
-        return this.zhemUserRestClient.findUserByPhone(phone);
-    }
-
-    @Override
-    public ZhemUserDto findAdmin() {
-        return this.zhemUserRestClient.findAdmin();
     }
 }
