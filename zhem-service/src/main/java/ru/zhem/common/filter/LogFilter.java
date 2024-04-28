ad452d08f427cbb2f98d7aaab2b7b7018d9e9b3d
hehmdalolkek <badeha.inna@yandex.ru>
2024-04-29 00:09:28 +0400

    added filter logger

---
+++ zhem-service/src/main/java/ru/zhem/common/filter/LogFilter.java
@@ -0,0 +1,22 @@
+package ru.zhem.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class LogFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        log.debug("Request {} {}: auth = {}", request.getMethod(), request.getRequestURI(), request.authenticate(response));
+        doFilter(request, response, filterChain);
+    }
+}
