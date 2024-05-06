db1d8e16810a4e2990c4a74f37302a0778300e0e
hehmdalolkek <badeha.inna@yandex.ru>
2024-05-06 15:10:30 +0400

    styled intervals

--- zhem-client/src/main/java/ru/zhem/controller/IntervalController.java
+++ zhem-client/src/main/java/ru/zhem/controller/IntervalController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.zhem.controller.util.ControllerUtil;
+import ru.zhem.service.interfaces.IntervalService;
 import ru.zhem.service.util.CalendarUtil;
 
 import java.time.YearMonth;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,12 +23,20 @@ public class IntervalController {
 
     private final CalendarUtil calendarUtil;
 
+    private final IntervalService intervalService;
+
     @GetMapping
     public String initShowAvailableIntervals(@RequestParam(value = "year", required = false) Integer year,
-                                             @RequestParam(value = "month", required = false) Integer month, Model model) {
+                                             @RequestParam(value = "month", required = false) Integer month, Model model,
+                                             Locale locale) {
         YearMonth yearMonth = this.calendarUtil.calcPrevNextMonth(model, year, month);
         model.addAttribute("mapOfIntervals",
                 this.controllerUtil.generateIntervalCalendarForMonth(yearMonth, true));
+        model.addAttribute("mapOfIntervalsIsEmpty",
+                this.intervalService.findAllAvailableIntervals(year, month).isEmpty());
+        String monthTitle = yearMonth.getMonth().getDisplayName(TextStyle.FULL_STANDALONE, locale);
+        monthTitle = monthTitle.replaceFirst(monthTitle.substring(0, 1), monthTitle.substring(0, 1).toUpperCase());
+        model.addAttribute("monthTitle", monthTitle);
         return "common/intervals/intervals";
     }
 
--- zhem-client/src/main/java/ru/zhem/controller/util/ControllerUtil.java
+++ zhem-client/src/main/java/ru/zhem/controller/util/ControllerUtil.java
@@ -4,12 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import ru.zhem.client.interfaces.AppointmentRestClient;
-import ru.zhem.client.interfaces.IntervalRestClient;
 import ru.zhem.dto.request.AppointmentDto;
 import ru.zhem.dto.request.DailyAppointmentDto;
 import ru.zhem.dto.request.DailyIntervalsDto;
 import ru.zhem.dto.request.IntervalDto;
+import ru.zhem.service.interfaces.AppointmentService;
+import ru.zhem.service.interfaces.IntervalService;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
@@ -19,9 +19,9 @@ import java.util.*;
 @Component
 public class ControllerUtil {
 
-    private final AppointmentRestClient appointmentRestClient;
+    private final AppointmentService appointmentService;
 
-    private final IntervalRestClient intervalRestClient;
+    private final IntervalService intervalService;
 
     public Map<String, String> getErrors(BindingResult bindingResult) {
         Map<String, String> errors = new HashMap<>();
@@ -41,7 +41,7 @@ public class ControllerUtil {
         }
 
         List<DailyAppointmentDto> dailyAppointments =
-                this.appointmentRestClient.findAllAppointments(yearMonth.getYear(), yearMonth.getMonthValue());
+                this.appointmentService.findAllAppointments(yearMonth.getYear(), yearMonth.getMonthValue());
         for (DailyAppointmentDto dailyAppointment : dailyAppointments) {
             mapOfAppointments.put(dailyAppointment.getDate(), dailyAppointment.getAppointments());
         }
@@ -55,19 +55,31 @@ public class ControllerUtil {
         LocalDate startOfMonth = yearMonth.atDay(1);
         LocalDate endOfMonth = yearMonth.atEndOfMonth();
 
+        int firstDayOfWeek = startOfMonth.getDayOfWeek().getValue();
+        YearMonth prevMonth = yearMonth.minusMonths(1);
+        for (int i = firstDayOfWeek - 2; i >= 0; i--) {
+            mapOfIntervals.put(prevMonth.atDay(prevMonth.lengthOfMonth() - i), new ArrayList<>());
+        }
+
         for (LocalDate date = startOfMonth; !date.isAfter(endOfMonth); date = date.plusDays(1)) {
             mapOfIntervals.put(date, new ArrayList<>());
         }
 
         List<DailyIntervalsDto> dailyIntervals;
         if (isAvailable) {
-            dailyIntervals = this.intervalRestClient.findAllAvailableIntervals(yearMonth.getYear(), yearMonth.getMonthValue());
+            dailyIntervals = this.intervalService.findAllAvailableIntervals(yearMonth.getYear(), yearMonth.getMonthValue());
         } else {
-            dailyIntervals = this.intervalRestClient.findAllIntervals(yearMonth.getYear(), yearMonth.getMonthValue());
+            dailyIntervals = this.intervalService.findAllIntervals(yearMonth.getYear(), yearMonth.getMonthValue());
         }
 
         for (DailyIntervalsDto dailyInterval : dailyIntervals) {
             mapOfIntervals.put(dailyInterval.getDate(), dailyInterval.getIntervals());
+        }
+
+        int lastDayOfWeek = endOfMonth.getDayOfWeek().getValue();
+        YearMonth nextMonth = yearMonth.plusMonths(1);
+        for (int i = 1; i <= 7 - lastDayOfWeek; i++) {
+            mapOfIntervals.put(nextMonth.atDay(i), new ArrayList<>());
         }
 
         return mapOfIntervals;
