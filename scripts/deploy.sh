b12cd2818242085ad5a1f358a8c812b05e226162
hehmdalolkek <badeha.inna@yandex.ru>
2024-05-13 19:04:15 +0400

    Revert "fix"
    This reverts commit 5522cfe72c76bb39723cff1eba947c5f40da4f79.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,14 @@ ssh hehmdalolkek@89.111.173.149 << EOF
 
 cd zhem-nails
 
-sudo docker-compose down
-sudo docker rmi zhem-nails-backend
-sudo docker rmi zhem-nails-client
+docker-compose down
+docker rmi zhem-nails-backend
+docker rmi zhem-nails-client
 
 git pull
 
 mvn clean package
 
-sudo docker-compose up -d
+docker-compose up -d
 
 EOF
