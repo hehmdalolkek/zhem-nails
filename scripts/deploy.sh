2779673d8603a156e71e1189cec852ad5fd78dfe
hehmdalolkek <badeha.inna@yandex.ru>
2024-05-13 14:55:36 +0400

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,11 @@
 
 ssh hehmdalolkek@178.208.94.26 << EOF
 
+cd app/zhem-nails
 docker compose down
-docker rm zhem_postgres
-docker rm zhem_service
-docker rm zhem_client
 docker rmi zhem-nails-backend
 docker rmi zhem-nails-client
 
-cd app/zhem-nails
 git pull
 cd zhem-service
 mvn clean package
